886a9bafcfee07403ad450f0d036e7eba8ed1339
Sarthak Dhingra <sartdh@hotmail.com>
2019-02-13 18:43:05 -0700

    removed numpy library

--- yield.py
+++ yield.py
@@ -1,6 +1,5 @@
 #import data anlysis libraries
 import pandas as pd
-import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from fancyimpute import KNN, IterativeImputer
